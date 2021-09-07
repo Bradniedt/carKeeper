c3e024c6eb6f5e3f7e288d55de74f81dba4f77e8
Bradniedt <bradleyniedt@gmail.com>
2021-09-07 07:42:38 -0600

    Adds initial commit for carKeeper.

---
+++ src/main/java/com/example/carKeeper/CarKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.example.carKeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carKeeper/CarKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carKeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
